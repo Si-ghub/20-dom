2163786070eeef7889cc404ab555c7325204744c
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-06-04 20:12:09 +0300

    Inner HTML

--- js/main.js
+++ js/main.js
@@ -88,56 +88,74 @@ hamburgerDOM.addEventListener('click', () => {
 })
 */
 
-/*UZDUOTIS 7
-Pradinis rezultatas lygus nuliui
+/*      //UZDUOTIS 7//                  I DALIS
+1. Pradinis rezultatas lygus nuliui
 
-Susirasti minuso mygtuka
+2. Susirasti minuso mygtuka
 
-Susirasti pliuso mygtuka
+3. Susirasti pliuso mygtuka
 
-Susirasti rezultato atvaizdavimo elementa
+4. Susirasti rezultato atvaizdavimo elementa
 
-Paspaudus ant minuso, rezultatas mazinamas vienu vienetu
+5. Paspaudus ant minuso, rezultatas mazinamas vienu vienetu
 
-Paspaudus ant pliuso, rezultatas didinamas vienu vienetu
+6. Paspaudus ant pliuso, rezultatas didinamas vienu vienetu
 
-Paspaudus ant reset, rezultatas turi grizti i nuli
+7. Paspaudus ant reset, rezultatas turi grizti i nuli
 
-Susirasti h1 elementa
+8. Susirasti h1 elementa
 
-Padaryti, jog paspaudus viena is mygtuku (pliusas, minusas) h1 elemento tekstas pasikeistu i "Žaidimas progrese"
+9 Padaryti, jog paspaudus viena is mygtuku (pliusas, minusas) h1 elemento tekstas pasikeistu i "Žaidimas progrese"
 
-Jei zaidimas yra nu'reset'inamas - h1 tekstas grizta i pradine reiksme
+10. Jei zaidimas yra nu'reset'inamas - h1 tekstas grizta i pradine reiksme
+
+/*                                       II DALIS
+1. Susirasti zinuciu rasymo elementa (ul)
+
+2. Kiekviena karta prasidedant zaidimui, reikia, jog zinuciu sarase atsirastu irasas su tekstu "Tu pasileidai zaidima - sekmes!"
+
+3. Kiekviena karta spaudziant minuso mygtuka, reikia, jog zinuciu sarase atsirastu irasas su tekstu "Paspaudei minusa ir dabartinis rezultatas yra [skaicius]."
+
+4. Kiekviena karta spaudziant pliuso mygtuka, reikia, jog zinuciu sarase atsirastu irasas su tekstu "Paspaudei pliusa ir dabartinis rezultatas yra [skaicius]."
+
+5. Kiekviena karta spaudziant reset mygtuka, reikia, jog zinuciu sarase atsirastu irasas su tekstu "Paspaudei reset ir dabartinis rezultatas yra [skaicius]."
 */
-//1 sprendimo budas
 
+//  SPRENDIMAS //
 
 let rezultatas = 0;
 const minusDOM = document.querySelector('.minus');
 const plusDOM = document.querySelector('.plus');
-const numberDOM = document.querySelector('.number');
-const h1DOM = document.querySelector('h1');
+const numberDOM = document.querySelector('.number');           // << susirandame visus kintamuosius
 const resetDOM = document.querySelector('.reset');
+const h1DOM = document.querySelector('h1');
+const ulDOM = document.querySelector('ul');
 
+const gameInProgressMessage = 'Žaidimas progrese';
+const initialH1Text = h1DOM.innerText;
+
+ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`; //  << issitraukiame vidini HTML teksta >>
 
 minusDOM.addEventListener('click', () => {
-    rezultatas = rezultatas - 1;              //gali buti naudojams --resultatas
+    rezultatas = rezultatas - 1;                             //g.b naudojamas --resultatas
     numberDOM.innerText = rezultatas;
-    h1DOM.innerText = 'Žaidimas progrese'
+    h1DOM.innerText = gameInProgressMessage;                //g.b stringas 'Žaidimas progrese'
+    ulDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${rezultatas}.</li>`;
 })
 
 plusDOM.addEventListener('click', () => {
-    rezultatas = rezultatas + 1;             //gali buti naudojama ++resultatas
+    rezultatas = rezultatas + 1;                           //g.b naudojama ++resultatas
     numberDOM.innerText = rezultatas;
-    h1DOM.innerText = 'Te prasideda žaidynės!'
+    h1DOM.innerText = gameInProgressMessage;               //g.b stringas 'Žaidimas progrese'
+    ulDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${rezultatas}.</li>`;
 })
 
 
 resetDOM.addEventListener('click', () => {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
-    h1DOM.innerText = ''
-
+    h1DOM.innerText = initialH1Text;                       //g.b stringas 'Te prasideda žaidynės!'
+    ulDOM.innerHTML += `<li>Paspaudei reset ir dabartinis rezultatas yra ${rezultatas}.</li>`
 })
 
 
