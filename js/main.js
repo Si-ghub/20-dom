cf67ae6fcefdc7667b85bec6309df99b11e656cb
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-06-15 19:40:25 +0300

    19 a+19 b solutions

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
-const optionDOM = document.querySelector('.option');
-const buttonDOM = document.querySelector('button');
-const inputDOM = document.getElementById('board');
-const boardDOM = document.querySelector('.board');
+function time(nuo, iki) {
+    for (let h = nuo; h < iki; h++) {
+        for (let m = 0; m < 60; m += 15) {
+            console.log(`${h}:${m}`);
 
-buttonDOM.addEventListener('click', (e) => {
-    e.preventDefault();
-    const n = parseInt(inputDOM.value);
-    optionDOM.textContent = `${n}x${n}`;
-    renderBoard(boardDOM, n);
-});
-
-function renderBoard(DOMelement, size) {
-    const elementSize = 100 / size;
-    let cellHTML = '';
-    let HTML = '';
-
-    for (let c = 0; c < size; c++) {
-        cellHTML += `<div class="cell" style="width: ${elementSize}%;"></div>`;
+        }
     }
-
-    for (let r = 0; r < size; r++) {
-        HTML += `<div class="row" style="height: ${elementSize}%;">${cellHTML}</div>`;
-    }
-
-    DOMelement.innerHTML = HTML;
 }
+time(1, 7);
+
+
+
+
+
+// let times = []
+//     , hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+//     , prop = null
+//     , hour = null
+//     , min = null;
+
+// for (hour in hours) {
+//     for (min = 0; min < 60; min += 15) {
+//         times.push(hours[hour].slice(-2)) + ':' + (min.slice(-2));
+
+//     }
+
+// }
+// console.log(times);
