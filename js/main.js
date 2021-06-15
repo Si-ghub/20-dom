174453a800570ab1fd5386fe7354fc3416e3e2ea
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-06-15 21:28:56 +0300

    19 c solution + 19 d and 20 tasks

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
-function time(nuo, iki) {
-    for (let h = nuo; h < iki; h++) {
-        for (let m = 0; m < 60; m += 15) {
-            console.log(`${h}:${m}`);
+function time(nuo, iki, intervalas) {
 
-        }
-    }
 }
-time(1, 7);
+time(1, 4, 31);
 
 
 
 
+/*
+let times = []
+    , hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    , prop = null
+    , hour = null
+    , min = null;
 
-// let times = []
-//     , hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-//     , prop = null
-//     , hour = null
-//     , min = null;
+for (hour in hours) {
+    for (min = 0; min < 60; min += 15) {
+        times.push(hours[hour].slice(-2)) + ':' + (min.slice(-2));
 
-// for (hour in hours) {
-//     for (min = 0; min < 60; min += 15) {
-//         times.push(hours[hour].slice(-2)) + ':' + (min.slice(-2));
+    }
 
-//     }
-
-// }
-// console.log(times);
+}
+console.log(times);
+*/
